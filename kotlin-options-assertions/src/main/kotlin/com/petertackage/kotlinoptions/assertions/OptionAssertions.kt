09deed29fc951529a889aae37e56b0d5c7e85f1c
Peter Tackage <peter.tackage@futurice.com>
2017-06-03 20:47:23 +0200

    Add assertion extensions API

--- kotlin-options-assertions/src/main/kotlin/com/petertackage/kotlinoptions/assertions/OptionAssertions.kt
+++ kotlin-options-assertions/src/main/kotlin/com/petertackage/kotlinoptions/assertions/OptionAssertions.kt
@@ -6,29 +6,47 @@ fun <T : Any> assertThat(actual: Option<T>): OptionAssertions<T> {
     return OptionAssertions(actual)
 }
 
+fun <T : Any> Option<T>.assertIsSome(): Option<T> {
+    return apply { assertThat(this).isSome() }
+}
+
+fun <T : Any> Option<T>.assertIsNone(): Option<T> {
+    return apply { assertThat(this).isNone() }
+}
+
+fun <T : Any> Option<T>.assertHasValue(expected: T): Option<T> {
+    return apply { assertThat(this).hasValue(expected) }
+}
+
+fun <T : Any> Option<T>.assertHasValue(predicate: (T) -> Boolean): Option<T> {
+    return apply { assertThat(this).hasValue(predicate) }
+}
+
 class OptionAssertions<T : Any> internal constructor(private val actual: Option<T>) {
 
     fun isSome(): OptionAssertions<T> {
-        assert(actual.isSome(), { "Expected: <Some> but was: <None>" })
-        return this
+        return apply {
+            assert(actual.isSome(),
+                    { "Expected: <Some> but was: <None>" })
+        }
     }
 
     fun isNone(): OptionAssertions<T> {
-        assert(actual.isNone(), { "Expected: <None> but was: <Some>" })
-        return this
+        return apply {
+            assert(actual.isNone(),
+                    { "Expected: <None> but was: <Some>" })
+        }
     }
 
     fun hasValue(expected: T): OptionAssertions<T> {
         isSome()
-        val actualValue = actual.getUnsafe()
-        assert(actualValue == expected, { "Expected value: <$expected> but was: <$actualValue>" })
-        return this
+        val actualValue: T = actual.getUnsafe()
+        return apply { assert(expected == actualValue, { "Expected value: <$expected> but was: <$actualValue>" }) }
     }
 
     fun hasValue(predicate: (T) -> Boolean): OptionAssertions<T> {
         isSome()
         val actualValue = actual.getUnsafe()
-        assert(predicate(actualValue), { "Expected predicate did not match: <$actualValue>" })
-        return this
+        return apply { assert(predicate(actualValue), { "Expected predicate did not match: <$actualValue>" }) }
     }
 }
--- kotlin-options-assertions/src/test/kotlin/com/petertackage/kotlinoptions/assertions/OptionAssertionsTest.kt
+++ kotlin-options-assertions/src/test/kotlin/com/petertackage/kotlinoptions/assertions/OptionAssertionsTest.kt
@@ -93,4 +93,95 @@ class OptionAssertionsTest {
         }
         assert(actualException.message == "Expected: <Some> but was: <None>")
     }
+
+    /*
+     * Tests for Extension methods
+     */
+
+    @Test
+    fun `assertIsSome passes when isSome`() {
+        val some = optionOf("abc")
+
+        some.assertIsSome()
+    }
+
+    @Test
+    fun `assertIsSome fails when isNone`() {
+        val actualException = assertFailsWith(AssertionError::class) {
+            Option.None.assertIsSome()
+        }
+        assert(actualException.message == "Expected: <Some> but was: <None>")
+    }
+
+    @Test
+    fun `assertIsNone passes when isNone`() {
+        Option.None.assertIsNone()
+    }
+
+    @Test
+    fun `assertIsNone fails when isSome`() {
+        val some = optionOf("abc")
+
+        val actualException = assertFailsWith(AssertionError::class) {
+            some.assertIsNone()
+        }
+        assert(actualException.message == "Expected: <None> but was: <Some>")
+    }
+
+    @Test
+    fun `assertHasValue passes when value equals`() {
+        val some = optionOf("abc")
+
+        some.assertHasValue("abc")
+    }
+
+    @Test
+    fun `assertHasValue fails when value not equals`() {
+        val some = optionOf("abc")
+
+        val actualException = assertFailsWith(AssertionError::class) {
+            some.assertHasValue("xyz")
+        }
+        assert(actualException.message == "Expected value: <xyz> but was: <abc>")
+    }
+
+    @Test
+    fun `assertHasValue fails when isNone`() {
+        val none = optionOf(null as String?)
+
+        val actualException = assertFailsWith(AssertionError::class) {
+            none.assertHasValue("does not matter")
+        }
+        assert(actualException.message == "Expected: <Some> but was: <None>")
+    }
+
+    @Test
+    fun `assertHasValue passes when value matches predicate`() {
+        val some = optionOf("abc")
+        val predicate: (String) -> Boolean = { true }
+
+        some.assertHasValue(predicate)
+    }
+
+    @Test
+    fun `assertHasValue fails when value does not match predicate`() {
+        val some = optionOf("abc")
+        val predicate: (String) -> Boolean = { false }
+
+        val actualException = assertFailsWith(AssertionError::class) {
+            some.assertHasValue(predicate)
+        }
+        assert(actualException.message == "Expected predicate did not match: <abc>")
+    }
+
+    @Test
+    fun `assertHasValue with predicate fails when isNone`() {
+        val none = optionOf(null as String?)
+        val predicate: (String) -> Boolean = { true }
+
+        val actualException = assertFailsWith(AssertionError::class) {
+            none.assertHasValue(predicate)
+        }
+        assert(actualException.message == "Expected: <Some> but was: <None>")
+    }
 }
