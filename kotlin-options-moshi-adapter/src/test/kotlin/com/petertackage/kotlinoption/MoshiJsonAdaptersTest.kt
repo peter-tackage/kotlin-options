5ab5c711297ecba143faa4fdd3077313b3040918
Peter Tackage <peter.tackage@gmail.com>
2017-06-15 13:26:16 +0200

    Moshi adapter (#10)

---
+++ kotlin-options-moshi-adapter/src/test/kotlin/com/petertackage/kotlinoption/MoshiJsonAdaptersTest.kt
@@ -0,0 +1,60 @@
+package com.petertackage.kotlinoption
+
+import com.petertackage.kotlinoptions.None
+import com.petertackage.kotlinoptions.Option
+import com.petertackage.kotlinoptions.optionOf
+import com.petertackage.kotlinoptions.samples.OptionAdapterFactory
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MoshiJsonAdaptersTest {
+
+    private lateinit var moshi: Moshi
+
+    data class Data(val optionField: Option<Int>)
+
+    data class DataWithDefault(val optionField: Option<Int> = None)
+
+    @Before
+    fun setUp() {
+        // Read about Kotlin handling: https://github.com/square/moshi/issues/143
+        moshi = Moshi.Builder()
+                .add(OptionAdapterFactory()) // must be first!
+                .add(KotlinJsonAdapterFactory()) // won't enforce missing fields unless present
+                .build()
+    }
+
+    @Test
+    fun `deserializes present field as optionOf value`() {
+        val jsonAdapter = moshi.adapter(Data::class.java)
+        val json = """ { "optionField" : 1 } """
+
+        val result = jsonAdapter.fromJson(json)!!
+
+        assertEquals(optionOf(1), result.optionField)
+    }
+
+    @Test
+    fun `deserializes null field as None`() {
+        val jsonAdapter = moshi.adapter(Data::class.java)
+        val json = """ { "optionField" : null } """
+
+        val result = jsonAdapter.fromJson(json)!!
+
+        assertEquals(None, result.optionField)
+    }
+
+    @Test
+    fun `deserializes missing field as None when default value`() {
+        val jsonAdapter = moshi.adapter(DataWithDefault::class.java)
+        val json = """ { } """
+
+        val result = jsonAdapter.fromJson(json)!!
+
+        assertEquals(None, result.optionField)
+    }
+
+}
