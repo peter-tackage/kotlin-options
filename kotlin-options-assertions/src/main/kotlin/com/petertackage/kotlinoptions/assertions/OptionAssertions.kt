64294c663eb53d3a34e17b7faec9c753de05eea2
Peter Tackage <peter.tackage@gmail.com>
2017-07-20 17:00:19 +0200

    Add top level Option docs (#20)

--- kotlin-options-assertions/src/main/kotlin/com/petertackage/kotlinoptions/assertions/OptionAssertions.kt
+++ kotlin-options-assertions/src/main/kotlin/com/petertackage/kotlinoptions/assertions/OptionAssertions.kt
@@ -4,28 +4,45 @@ import com.petertackage.kotlinoptions.None
 import com.petertackage.kotlinoptions.Option
 import com.petertackage.kotlinoptions.Some
 
+/**
+ * Creates a new instance of [OptionAssertions] to allow asserting on the properties of the instance under test: [actual].
+ */
 fun <T : Any> assertThat(actual: Option<T>): OptionAssertions<T> {
     return OptionAssertions(actual)
 }
 
+/**
+ * Asserts that the instance under test is [Some].
+ */
 fun <T : Any> Option<T>.assertIsSome(): Option<T> {
     return apply { assertThat(this).isSome() }
 }
 
+/**
+ * Asserts that the instance under test is [None].
+ */
 fun <T : Any> Option<T>.assertIsNone(): Option<T> {
     return apply { assertThat(this).isNone() }
 }
 
+/**
+ * Asserts that the instance under test is [Some] and has the value [expected].
+ */
 fun <T : Any> Option<T>.assertHasValue(expected: T): Option<T> {
     return apply { assertThat(this).hasValue(expected) }
 }
-
-fun <T : Any> Option<T>.assertHasValue(predicate: (T) -> Boolean): Option<T> {
-    return apply { assertThat(this).hasValue(predicate) }
+/**
+ * Asserts that the instance under test is [Some] and has the value returned by function [expectedPredicate].
+ */
+fun <T : Any> Option<T>.assertHasValue(expectedPredicate: (T) -> Boolean): Option<T> {
+    return apply { assertThat(this).hasValue(expectedPredicate) }
 }
 
 class OptionAssertions<T : Any> internal constructor(private val actual: Option<T>) {
 
+    /**
+     * Asserts that the instance under test is [Some].
+     */
     fun isSome(): OptionAssertions<T> {
         return apply {
             assert(actual is Some,
@@ -33,19 +50,26 @@ class OptionAssertions<T : Any> internal constructor(private val actual: Option<
         }
     }
 
+    /**
+     * Asserts that the instance under test is [None].
+     */
     fun isNone(): OptionAssertions<T> {
         return apply {
             assert(actual is None,
                     { "Expected: <None> but was: <Some>" })
         }
     }
-
+    /**
+     * Asserts that the instance under test is [Some] and has the value [expected].
+     */
     fun hasValue(expected: T): OptionAssertions<T> {
         isSome()
         val actualValue: T = actual.getUnsafe()
         return apply { assert(expected == actualValue, { "Expected value: <$expected> but was: <$actualValue>" }) }
     }
-
+    /**
+     * Asserts that the instance under test is [Some] and has the value returned by function [expectedPredicate].
+     */
     fun hasValue(expectedPredicate: (T) -> Boolean): OptionAssertions<T> {
         isSome()
         val actualValue = actual.getUnsafe()
--- kotlin-options/src/main/kotlin/com/petertackage/kotlinoptions/IterableExtensions.kt
+++ kotlin-options/src/main/kotlin/com/petertackage/kotlinoptions/IterableExtensions.kt
@@ -1,9 +1,15 @@
 package com.petertackage.kotlinoptions
 
+/**
+ * @return an [Iterable] containing all elements of the original [Iterable] which are [Some].
+ */
 fun <T : Any> Iterable<Option<T>>.filterIfSome(): Iterable<T> {
     return filter { it is Some }.map { it.toNullable()!! }
 }
 
+/**
+ * @return an [Iterable] containing all elements of the original [Iterable] which are [Some] and that satisfy [predicate].
+ */
 fun <T : Any> Iterable<Option<T>>.filterIfSome(predicate: (T) -> Boolean): Iterable<T> {
     return filter { it is Some }.map { it.toNullable()!! }.filter(predicate)
 }
