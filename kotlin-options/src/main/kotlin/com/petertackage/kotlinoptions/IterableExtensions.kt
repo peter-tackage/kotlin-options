2e84746a272f23ecb1516c6eafd5cdbf64081a59
Peter Tackage <peter.tackage@futurice.com>
2017-07-20 16:02:17 +0200

    Clarify docs for iterable extensions

--- kotlin-options/src/main/kotlin/com/petertackage/kotlinoptions/IterableExtensions.kt
+++ kotlin-options/src/main/kotlin/com/petertackage/kotlinoptions/IterableExtensions.kt
@@ -1,14 +1,14 @@
 package com.petertackage.kotlinoptions
 
 /**
- * @return an [Iterable] containing all elements of the original collection which are [Some].
+ * @return an [Iterable] containing all elements of the original [Iterable] which are [Some].
  */
 fun <T : Any> Iterable<Option<T>>.filterIfSome(): Iterable<T> {
     return filter { it is Some }.map { it.toNullable()!! }
 }
 
 /**
- * @return an [Iterable] containing all elements of the original collection which are [Some] and that satisfy [predicate].
+ * @return an [Iterable] containing all elements of the original [Iterable] which are [Some] and that satisfy [predicate].
  */
 fun <T : Any> Iterable<Option<T>>.filterIfSome(predicate: (T) -> Boolean): Iterable<T> {
     return filter { it is Some }.map { it.toNullable()!! }.filter(predicate)
